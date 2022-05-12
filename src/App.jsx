e4bb52402f1ee7d255cc3def95e0036761a37077
pandacover <luvmakin01@gmail.com>
2022-05-12 14:38:30 +0530

    initialized with cra template and tailwindcss
    starting point created with cra

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+	return (
+		<div className='w-screen min-h-screen overflow-x-hidden relative'>
+			<h1 className='font-black text-6xl tracking-wider'>Hello React + Tailwindcss</h1>
+		</div>
+	)
+}
+
+export default App
