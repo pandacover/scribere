24893080f1cbfd0b6f69178f8cbb613d176e1321
AayushThakur1999 <ashcold21@gmail.com>
2022-05-12 18:48:54 +0530

    Test Change 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ const App = () => {
 	return (
 		<div className='w-screen min-h-screen overflow-x-hidden relative'>
 			<h1 className='font-black text-6xl tracking-wider'>Hello React + Tailwindcss</h1>
+			<h1 className="text-center font-black">Scribere</h1>
 		</div>
 	)
 }
