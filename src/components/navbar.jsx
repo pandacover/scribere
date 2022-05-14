73810ba75088f42772f387bd48de7ae815a5bc4a
pandacover <luvmakin01@gmail.com>
2022-05-14 17:35:07 +0530

    Edited App.jsx | All three files in Components folder | added index.js for better importing | edited tailwind.config.js and gloabals.css stylesheet / layout, navbar, sidebar and main component | extended styles in tailwind.config.js and globals.css

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -16,24 +16,24 @@ const Navbar = () => {
   }
 
   return (
-    <header className='flex justify-between items-center w-full h-16 px-6'>
-      <p className='lowercase text-4xl font-medium font-serif flex items-center gap-2'>
+    <header className='border dark:border-zinc-800 flex justify-between items-center w-full h-16 px-6'>
+      <p className='lowercase text-4xl font-medium font-serif flex items-center gap-2 dark:text-white'>
         <span>Scrībere</span>
       </p>
       <div className='flex-1 flex justify-end gap-8 items-center'>
         <button className='w-5 h-3 rounded-xl bg-gray-300 dark:bg-gray-400 flex items-center relative' onClick={(e) => toggleDarkMode(e)}>
           <div className={`toggle-btn w-4 h-4 bg-gray-400 rounded-full absolute ${toggleMode ? 'toggled' : ''}`} />
         </button>
-        <div className='select-none relative flex items-center p-2 group hover:bg-black transition cursor-pointer' onClick={e => handleToggleMenu(e)}>
+        <div className={`select-none relative flex items-center p-2 group hover:bg-surface-black transition cursor-pointer ${toggleMenu ? 'bg-surface-black' : ''}`} onClick={e => handleToggleMenu(e)}>
           <div className='flex gap-2 items-center'>
             <img className='w-8 h-8 rounded-full' src='https://avatars.dicebear.com/api/male/john.svg?background=%230000ff' alt='user profile avatar' />
             <figure>
-              <svg className='group-hover:translate-y-2 fill-black group-hover:fill-white transition' width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <svg className='group-hover:translate-y-2 fill-black group-hover:fill-white dark:fill-white dark:group-hover:fill-gray-400 transition' width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M16.9999 9.17019C16.8126 8.98394 16.5591 8.87939 16.2949 8.87939C16.0308 8.87939 15.7773 8.98394 15.5899 9.17019L11.9999 12.7102L8.45995 9.17019C8.27259 8.98394 8.01913 8.87939 7.75495 8.87939C7.49076 8.87939 7.23731 8.98394 7.04995 9.17019C6.95622 9.26315 6.88183 9.37375 6.83106 9.49561C6.78029 9.61747 6.75415 9.74818 6.75415 9.88019C6.75415 10.0122 6.78029 10.1429 6.83106 10.2648C6.88183 10.3866 6.95622 10.4972 7.04995 10.5902L11.2899 14.8302C11.3829 14.9239 11.4935 14.9983 11.6154 15.0491C11.7372 15.0998 11.8679 15.126 11.9999 15.126C12.132 15.126 12.2627 15.0998 12.3845 15.0491C12.5064 14.9983 12.617 14.9239 12.7099 14.8302L16.9999 10.5902C17.0937 10.4972 17.1681 10.3866 17.2188 10.2648C17.2696 10.1429 17.2957 10.0122 17.2957 9.88019C17.2957 9.74818 17.2696 9.61747 17.2188 9.49561C17.1681 9.37375 17.0937 9.26315 16.9999 9.17019Z" />
               </svg>
             </figure>
           </div>
-          <div className={`transition absolute w-48 bg-zinc-900 scale-0 text-white -left-[104px] top-16 p-2 grid grid-flow-row ${toggleMenu ? 'scale-100' : ''}`}>
+          <div className={`transition absolute w-48 bg-surface-black scale-0 text-white -left-[111px] top-[50px] p-2 grid grid-flow-row ${toggleMenu ? 'scale-100' : ''}`}>
             <div className='flex items-center gap-4'>
               <figure className='w-8 h-8'>
                 <img className='object-contain' src='https://avatars.dicebear.com/api/male/john.svg?background=%230000ff' alt='user profile avatar' />
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Sidebar = () => {
     return (
-        <main>
-            <h1>Sidebar</h1>
-        </main>
+        <div className='p-4 border dark:border-zinc-800 dark:text-white h-container-height -mt-[1px] flex-1 w-full'>
+            <div className='flex items-center justify-center'>
+                <button className='font-medium rounded-md border-2 border-black hover:bg-black hover:text-white dark:border-white dark:hover:bg-white dark:hover:text-black px-4 py-2 transition-all duration-150' type='button'>Create Note</button>
+            </div>
+        </div>
     )
 }
 
